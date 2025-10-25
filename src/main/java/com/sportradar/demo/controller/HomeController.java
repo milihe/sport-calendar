1e00f4af137790f6838779438bfec9e6bf5db747
Milla Herth <kuruczmilla@gmail.com>
2025-10-25 16:17:13 +0200

    get sports from db

--- src/main/java/com/sportradar/demo/controller/HomeController.java
+++ src/main/java/com/sportradar/demo/controller/HomeController.java
@@ -1,6 +1,9 @@
 package com.sportradar.demo.controller;
 
 import com.sportradar.demo.Sport;
+import com.sportradar.demo.repository.SportRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.jdbc.core.JdbcTemplate;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -11,12 +14,13 @@ import java.util.List;
 @Controller
 public class HomeController {
 
+    @Autowired
+    JdbcTemplate jdbc;
+
     @GetMapping("/")
     public String home(Model model) {
-        List<Sport> sports = List.of(
-                new Sport(1, "handball"),
-                new Sport(2, "football")
-        );
+        var sportRepository = new SportRepository(jdbc);
+        var sports = sportRepository.getSports();
 
         model.addAttribute("sports", sports);
         return "home"; // This resolves to src/main/resources/templates/home.html
---
+++ src/main/java/com/sportradar/demo/repository/SportRepository.java
@@ -0,0 +1,26 @@
+package com.sportradar.demo.repository;
+
+import com.sportradar.demo.Sport;
+import org.springframework.jdbc.core.JdbcTemplate;
+
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.util.List;
+
+public class SportRepository {
+    private final JdbcTemplate jdbc;
+
+    public SportRepository(JdbcTemplate jdbc) {
+        this.jdbc = jdbc;
+    }
+
+    public List<Sport> getSports() {
+        return jdbc.query("SELECT * FROM sports ORDER BY sport", this::mapRow);
+    }
+
+    private Sport mapRow(ResultSet rs, int rowNum) throws SQLException {
+        return new Sport(
+           rs.getInt("sportId"), rs.getString("sport")
+        );
+    }
+}
