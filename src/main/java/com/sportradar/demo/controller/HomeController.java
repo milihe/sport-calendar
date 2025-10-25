dfd9e2ae6ef2da9382e72fa1e5cf05d1dedc8c94
Milla Herth <kuruczmilla@gmail.com>
2025-10-25 15:43:49 +0200

    print sports in the home template

--- src/main/java/com/sportradar/demo/controller/HomeController.java
+++ src/main/java/com/sportradar/demo/controller/HomeController.java
@@ -1,16 +1,24 @@
 package com.sportradar.demo.controller;
 
+import com.sportradar.demo.Sport;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+
+import java.util.ArrayList;
+import java.util.List;
 
 @Controller
 public class HomeController {
 
     @GetMapping("/")
     public String home(Model model) {
+        List<Sport> sports = List.of(
+                new Sport(1, "handball"),
+                new Sport(2, "football")
+        );
 
-        model.addAttribute("message", "Hello from Thymeleaf!");
+        model.addAttribute("sports", sports);
         return "home"; // This resolves to src/main/resources/templates/home.html
     }
 }
