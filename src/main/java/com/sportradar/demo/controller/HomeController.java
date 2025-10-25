89331de0cae9f5423a9523fa3667826a7efc60b7
Milla Herth <kuruczmilla@gmail.com>
2025-10-25 16:30:38 +0200

    add events to template

--- src/main/java/com/sportradar/demo/controller/HomeController.java
+++ src/main/java/com/sportradar/demo/controller/HomeController.java
@@ -1,5 +1,6 @@
 package com.sportradar.demo.controller;
 
+import com.sportradar.demo.Event;
 import com.sportradar.demo.Sport;
 import com.sportradar.demo.repository.SportRepository;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -8,6 +9,7 @@ import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 
+import java.time.Instant;
 import java.util.ArrayList;
 import java.util.List;
 
@@ -21,8 +23,13 @@ public class HomeController {
     public String home(Model model) {
         var sportRepository = new SportRepository(jdbc);
         var sports = sportRepository.getSports();
-
         model.addAttribute("sports", sports);
+
+        var events = List.of(
+                new Event(1, Instant.now(), "handball", "Gyor", "Fradi"),
+                new Event(2, Instant.now(), "handball", "Bp", "Fradi")
+                );
+        model.addAttribute("events", events);
         return "home"; // This resolves to src/main/resources/templates/home.html
     }
 }
