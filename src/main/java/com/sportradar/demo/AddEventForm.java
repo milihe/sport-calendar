8a45f0b959b436c36341695b16a9d013d4491c21
Milla Herth <kuruczmilla@gmail.com>
2025-10-28 12:23:30 +0100

    improve add event controller

---
+++ src/main/java/com/sportradar/demo/AddEventForm.java
@@ -0,0 +1,113 @@
+package com.sportradar.demo;
+
+import com.sportradar.demo.repository.SportRepository;
+import com.sportradar.demo.repository.TeamRepository;
+import org.springframework.ui.Model;
+
+import java.time.Instant;
+import java.time.LocalDateTime;
+import java.time.ZoneId;
+import java.util.ArrayList;
+import java.util.List;
+
+public class AddEventForm {
+    private int sportId;
+    private String localStart;
+    private Instant start;
+    private int teamId1;
+    private int teamId2;
+
+    public AddEventForm(String paramSportId) {
+        sportId = tryParseInteger(paramSportId, 0);
+    }
+
+    public AddEventForm(String paramSportId, String paramLocalStart, String timeZoneId, String paramTeamId1, String paramTeamId2) {
+        sportId = tryParseInteger(paramSportId, 0);
+        localStart = paramLocalStart;
+        start = tryParseInstant(paramLocalStart, timeZoneId);
+        teamId1 = tryParseInteger(paramTeamId1, 0);
+        teamId2 = tryParseInteger(paramTeamId2, 0);
+    }
+
+    public int getSportId() {
+        return sportId;
+    }
+
+    public Instant getStart() {
+        return start;
+    }
+
+    public int getTeamId1() {
+        return teamId1;
+    }
+
+    public int getTeamId2() {
+        return teamId2;
+    }
+
+    public List<String> getValidationErrors() {
+        var errors = new ArrayList<String>();
+        if (sportId == 0) {
+            errors.add("Sport is not selected.");
+        }
+        if (start == null) {
+            errors.add("Start is not selected.");
+        }
+        if (teamId1 == 0) {
+            errors.add("Team1 is not selected");
+        }
+        if (teamId2 == 0) {
+            errors.add("Team2 is not selected");
+        }
+        if (teamId1 > 0 && teamId1 == teamId2) {
+            errors.add("Same team cannot be selected twice");
+        }
+        return errors;
+    }
+
+    public void setupUIModel(Model model, SportRepository sportRepository, TeamRepository teamRepository, List<String> errors) {
+        // To populate the sport selector dropdown list
+        var sport = sportRepository.getSports();
+        model.addAttribute("sports", sport);
+
+        // To populate the team selector dropdown list
+        if (sportId == 0) {
+            // When navigating to the add event page, the sportId will be null,
+            // in that case we are going to autoselect the first sport, so we can populate the available teams accordingly.
+            sportId = sport.getFirst().getSportId();
+        }
+        var teams = teamRepository.getTeamsBySportId(sportId);
+        model.addAttribute("teams", teams);
+
+        // In case the validation fails for adding event, and form gets displayed again,
+        // we will send back the current values, so we can restore the state of inputs, dropdowns
+        model.addAttribute("selectedSportId", sportId);
+        model.addAttribute("start", localStart);
+        model.addAttribute("teamId1", teamId1);
+        model.addAttribute("teamId2", teamId2);
+
+        if (errors != null && errors.size() > 0) {
+            model.addAttribute("error", String.join(" ",errors));
+        }
+
+    }
+
+    private int tryParseInteger(String value, int defaultValue) {
+        try {
+            return Integer.parseInt(value);
+        } catch (Exception ex) {
+            return defaultValue;
+        }
+    }
+
+    private Instant tryParseInstant(String localDateTimeValue, String timeZoneId) {
+        try {
+            var localDateTime = LocalDateTime.parse(localDateTimeValue);
+            return localDateTime.atZone(ZoneId.of(timeZoneId)).toInstant();
+        } catch (Exception ex) {
+            return null;
+        }
+    }
+
+
+}
--- src/main/java/com/sportradar/demo/controller/AddEventController.java
+++ src/main/java/com/sportradar/demo/controller/AddEventController.java
@@ -1,5 +1,6 @@
 package com.sportradar.demo.controller;
 
+import com.sportradar.demo.AddEventForm;
 import com.sportradar.demo.repository.EventRepository;
 import com.sportradar.demo.repository.SportRepository;
 import com.sportradar.demo.repository.TeamRepository;
@@ -16,16 +17,15 @@ import java.time.ZoneId;
 
 @Controller
 public class AddEventController {
-    private final static int unselectedValue = -1;
-
     @Autowired
-    JdbcTemplate jdbc;
+    JdbcTemplate db;
 
     @GetMapping("/add-event")
     public String addEvent(Model model,
                            @RequestParam(value = "sportId", required = false) String paramSportId) {
 
-        initializeModelForInputForm(model, paramSportId, null, null, null, null);
+        var addEventForm = new AddEventForm(paramSportId);
+        addEventForm.setupUIModel(model, new SportRepository(db), new TeamRepository(db), null);
         return "add-event";
     }
 
@@ -36,68 +36,20 @@ public class AddEventController {
                          @RequestParam(value = "teamId1") String paramTeamId1,
                          @RequestParam(value = "teamId2") String paramTeamId2) {
 
-        var eventRepository = new EventRepository(jdbc);
-        try {
-            var sportId = Integer.parseInt(paramSportId);
-
-            if (paramStart.isEmpty()) {
-                throw new IllegalArgumentException("Start is not selected");
-            }
-            var localStart = LocalDateTime.parse(paramStart);
-            var start = localStart.atZone(ZoneId.of("Europe/Vienna")).toInstant();
-
-            var teamId1 = Integer.parseInt(paramTeamId1);
-            var teamId2 = Integer.parseInt(paramTeamId2);
-            if (teamId1 == teamId2) {
-                throw new IllegalArgumentException("Same team cannot be selected twice");
-            }
-
-            eventRepository.addEvent(start, sportId, teamId1, teamId2);
-        } catch (Exception ex) {
-            var error = ex.getMessage();
-            initializeModelForInputForm(model, paramSportId, paramStart, paramTeamId1, paramTeamId2, error);
+        var addEventForm = new AddEventForm(paramSportId, paramStart, "Europe/Vienna", paramTeamId1, paramTeamId2);
+        var validationErrors = addEventForm.getValidationErrors();
+        if (validationErrors.isEmpty()) {
+            var eventRepository = new EventRepository(db);
+            eventRepository.addEvent(addEventForm.getStart(),
+                    addEventForm.getSportId(),
+                    addEventForm.getTeamId1(),
+                    addEventForm.getTeamId2());
+            // after the event was inserted to db, we redirect back to the page listing the events.
+            return "redirect:/";
+        } else {
+            // validation failed, we show the form again
+            addEventForm.setupUIModel(model, new SportRepository(db), new TeamRepository(db), validationErrors);
             return "add-event";
-        }
-
-        return "redirect:/";
-    }
-
-    private void initializeModelForInputForm(Model model,
-                                        String sportId,
-                                        String start,
-                                        String teamId1,
-                                        String teamId2,
-                                        String error) {
-
-        var sportRepository = new SportRepository(jdbc);
-        var sports = sportRepository.getSports();
-        // to populate the dropdown for selecting the sport
-        model.addAttribute("sports", sports);
-
-        int selectedSportId = parseIntegerParameter(sportId, unselectedValue);
-        if (selectedSportId == unselectedValue) {
-            selectedSportId = sports.getFirst().getSportId();
-        }
-        model.addAttribute("selectedSportId", selectedSportId);
-
-        // to populate the dropdowns for selecting the teams
-        var teamRepository = new TeamRepository(jdbc);
-        var teams = teamRepository.getTeamsBySportId(selectedSportId);
-        model.addAttribute("teams", teams);
-
-        // if the insert is rejected (validation error),
-        // we want to preserve the state of the form
-        model.addAttribute("start", start);
-        model.addAttribute("teamId1", teamId1);
-        model.addAttribute("teamId2", teamId2);
-        model.addAttribute("error", error);
-    }
-
-    private static int parseIntegerParameter(String value, int defaultValue) {
-        try {
-            return Integer.parseInt(value);
-        } catch (Exception ex) {
-            return defaultValue;
         }
     }
 
